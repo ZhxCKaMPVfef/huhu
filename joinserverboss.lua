59ec3827e3b153f69f69d1ad8d3bd1ee51af08ea
Hlamx <88639120+hlamx@users.noreply.github.com>
2023-11-26 20:56:27 +0700

    Update joinserverboss.lua

--- joinserverboss.lua
+++ joinserverboss.lua
@@ -1,10 +1,11 @@
+local teams = "Marines"
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer:FindFirstChild("DataLoaded")
 repeat
     pcall(
         function()
             task.wait()
             if game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("Main"):FindFirstChild("ChooseTeam") then
-                if string.find(tostring(h["Team"]), "Pirate") then
+                if string.find(teams, "Pirate") then
                     for r, v in pairs(
                         getconnections(
                             game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Pirates.Frame.ViewportFrame.TextButton.Activated
@@ -12,7 +13,7 @@ repeat
                     ) do
                         v.Function()
                     end
-                elseif string.find(tostring(h["Team"]), "Marine") then
+                elseif string.find(teams, "Marine") then
                     for r, v in pairs(
                         getconnections(
                             game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Marines.Frame.ViewportFrame.TextButton.Activated
