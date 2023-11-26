75df85cf1887cee9c84b15fffe380dd6312d46ee
Hlamx <88639120+hlamx@users.noreply.github.com>
2023-11-26 20:55:48 +0700

    Update joinserverboss.lua

--- joinserverboss.lua
+++ joinserverboss.lua
@@ -75,20 +75,24 @@ if game.PlaceId == 7449423635 then
     end
 
     while wait() do
-        if v5() and not game:GetService("Workspace").Map:FindFirstChild("MysticIsland") and not game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("CommF_"):InvokeServer("CheckTempleDoor") and ( function7() == "Night" or function6() < 5 ) then
-            local response = getinfoall()
-            if response and #response > 0 then
-                local latestEntry = response[#response]
-                local playingValue = tonumber(latestEntry.Playing)
-                print(latestEntry.Text)
+        repeat
+            wait()
+            if v5() and not game:GetService("Workspace").Map:FindFirstChild("MysticIsland") and not game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("CommF_"):InvokeServer("CheckTempleDoor") and (function7() == "Night" or function6() < 5) then
+                local response = getinfoall()
+                if response and #response > 0 then
+                    local latestEntry = response[#response]
+                    local playingValue = tonumber(latestEntry.Playing)
+                    print(latestEntry.Text)
 
-                if latestEntry.Text == "Mirage Island :island: | :crescent_moon: Night" and playingValue and playingValue < 12 then
-                    game:GetService("TeleportService"):TeleportToPlaceInstance(latestEntry.PlaceId, latestEntry.JobId,
-                        game.Players.LocalPlayer)
+                    if latestEntry.Text == "Mirage Island :island: | :crescent_moon: Night" and playingValue and playingValue < 12 then
+                        game:GetService("TeleportService"):TeleportToPlaceInstance(latestEntry.PlaceId, latestEntry
+                            .JobId,
+                            game.Players.LocalPlayer)
+                    end
+                else
+                    print("Dữ liệu rỗng hoặc không có dữ liệu trả về từ API")
                 end
-            else
-                print("Dữ liệu rỗng hoặc không có dữ liệu trả về từ API")
             end
-        end
+        until not game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("CommF_"):InvokeServer("CheckTempleDoor")
     end
 end
