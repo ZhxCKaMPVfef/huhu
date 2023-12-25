1dc06833b3a0432a7970ae7d31664211fb905908
Hlamx <88639120+hlamx@users.noreply.github.com>
2023-12-25 15:26:55 +0700

    Update storefruit.lua

--- storefruit.lua
+++ storefruit.lua
@@ -1,4 +1,33 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer:FindFirstChild("DataLoaded")
+spawn(function()
+    while wait() do
+        if not game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("MyGUIFrame") then
+            local player = game.Players.LocalPlayer
+            local playerGui = player:WaitForChild("PlayerGui")
+
+            local frameSize = 50 -- Kích thước của frame (vuông)
+            local guiFrame = Instance.new("ImageButton")
+            guiFrame.Name = "MyGUIFrame"
+            guiFrame.Size = UDim2.new(0, frameSize, 0, frameSize) -- Kích thước cố định
+            guiFrame.BackgroundColor3 = Color3.new(0, 0, 0)       -- Màu nền của frame
+            guiFrame.Visible = true                               -- Ẩn frame ban đầu
+
+            local function updateFramePosition(mousePosition)
+                local halfSize = frameSize / 2
+                guiFrame.Position = UDim2.new(mousePosition.X, 0, mousePosition.Y, 0)
+            end
+
+            game:GetService("RunService").RenderStepped:Connect(function()
+                local mouse = game.Players.LocalPlayer:GetMouse()
+                local mouseHit = mouse.Hit
+                updateFramePosition(mouseHit.Position)
+            end)
+
+
+            guiFrame.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+        end
+    end
+end)
 local VirtualUser = game:service 'VirtualUser'
 game:service('Players').LocalPlayer.Idled:connect(function()
     VirtualUser:CaptureController()
