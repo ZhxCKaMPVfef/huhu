3f813010aff6e3ef3956af0cc7732747a97fa41d
Hlamx <88639120+hlamx@users.noreply.github.com>
2024-01-14 11:53:08 +0700

    Update storefruit.lua

--- storefruit.lua
+++ storefruit.lua
@@ -47,20 +47,28 @@ function clickUI(a)
     (game:GetService("VirtualInputManager")):SendMouseButtonEvent(a.AbsolutePosition.X + a.AbsoluteSize.X / 2,
         a.AbsolutePosition.Y + 50, 0, false, a, 1);
 end;
-
-for i, v in next, game:GetService("Players").LocalPlayer.Backpack:GetChildren() do
-    if not game:GetService("Players").LocalPlayer.PlayerGui.Stats.Button.Inventory_Frame.ScrollingFrameFruits:FindFirstChild(v.Name) then
-        print("Fruit Dont Have In Bag")
-        if string.find(v.Name, "Fruit") then
-            EquipWeapon(tostring(v.Name))
-            wait(2)
-            clickUI(game:GetService("Players").LocalPlayer.PlayerGui.MyGUIFrame)
-            for i1, v1 in next, game:GetService("Players").LocalPlayer.PlayerGui:GetChildren() do
-                if string.find(v1.Name, "EatFruit") then
-                    repeat
-                        wait()
-                        clickUI(v1.Dialogue.Collect)
-                    until not string.find(v1.Name, "EatFruit") or not v1 or not v1.Parent
+function gethavefruit()
+    for i, v in next, game:GetService("Players").LocalPlayer.Backpack:GetChildren() do
+        if string.find(v.Name, "Fruit") and not game:GetService("Players").LocalPlayer.PlayerGui.Stats.Button.Inventory_Frame.ScrollingFrameFruits:FindFirstChild(v.Name) then
+            return true
+        end
+    end
+end
+while wait() do 
+    if gethavefruit() then 
+    for i, v in next, game:GetService("Players").LocalPlayer.Backpack:GetChildren() do
+            print("Fruit Dont Have In Bag")
+            if string.find(v.Name, "Fruit") then
+                EquipWeapon(tostring(v.Name))
+                wait(2)
+                clickUI(game:GetService("Players").LocalPlayer.PlayerGui.MyGUIFrame)
+                for i1, v1 in next, game:GetService("Players").LocalPlayer.PlayerGui:GetChildren() do
+                    if string.find(v1.Name, "EatFruit") then
+                        repeat
+                            wait()
+                            clickUI(v1.Dialogue.Collect)
+                        until not string.find(v1.Name, "EatFruit") or not v1 or not v1.Parent or not gethavefruit()
+                    end
                 end
             end
         end
