a57b90c7bb3fb77029f50467fd2cf6db69a188ed
Hlamx <88639120+hlamx@users.noreply.github.com>
2024-04-03 13:42:43 +0700

    Update misc.lua

--- misc.lua
+++ misc.lua
@@ -1,4 +1,4 @@
-getgenv().link = "http://100.126.11.57:3000/mirage"
+getgenv().link = "http://192.168.1.2:3000/mirage"
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer:FindFirstChild("DataLoaded")
 while wait() do
     if game.Players.LocalPlayer.Team ~= nil then
