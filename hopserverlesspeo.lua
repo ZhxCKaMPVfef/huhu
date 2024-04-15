8185e6b29c19914ca912f2fa7a323956d2483820
Hlamx <88639120+hlamx@users.noreply.github.com>
2024-04-15 09:06:05 +0700

    Update hopserverlesspeo.lua

--- hopserverlesspeo.lua
+++ hopserverlesspeo.lua
@@ -1,45 +1,71 @@
+repeat wait() until game:IsLoaded()
 local PlaceID = game.PlaceId
 local AllIDs = {}
 local foundAnything = ""
 local actualHour = os.date("!*t").hour
 local Deleted = false
-
-local function tpserverless()
-    local Sitez;
+local File = pcall(function()
+    AllIDs = game:GetService('HttpService'):JSONDecode(readfile("NotSameServers.json"))
+end)
+if not File then
+    table.insert(AllIDs, actualHour)
+    writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
+end
+function TPReturner()
+    local Site;
     if foundAnything == "" then
-        Sitez = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' ..
-            PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
+        Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' ..
+        PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
     else
-        Sitez = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' ..
-            PlaceID .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
+        Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' ..
+        PlaceID .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
     end
     local ID = ""
-    if Sitez.nextPageCursor and Sitez.nextPageCursor ~= "null" and Sitez.nextPageCursor ~= nil then
-        foundAnything = Sitez.nextPageCursor
+    if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+        foundAnything = Site.nextPageCursor
     end
     local num = 0;
-    for i, v in pairs(Sitez.data) do
+    for i, v in pairs(Site.data) do
         local Possible = true
         ID = tostring(v.id)
-        if tonumber(v.playing) < tonumber(v.maxPlayers) then -- Change to '<' instead of '<='
+        if tonumber(v.maxPlayers) > tonumber(v.playing) then
             for _, Existing in pairs(AllIDs) do
-                if ID == tostring(Existing) then
-                    Possible = false
-                    break -- Break out of the loop once a match is found
+                if num ~= 0 then
+                    if ID == tostring(Existing) then
+                        Possible = false
+                    end
+                else
+                    if tonumber(actualHour) ~= tonumber(Existing) then
+                        local delFile = pcall(function()
+                            AllIDs = {}
+                            table.insert(AllIDs, actualHour)
+                        end)
+                    end
                 end
+                num = num + 1
             end
-            if Possible then
+            if Possible == true then
                 table.insert(AllIDs, ID)
+                wait()
                 pcall(function()
-                    writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
+                    wait()
                     game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
                 end)
-                break -- Break out of the loop once a valid server is found
+                wait(4)
             end
-        else
-            print("Server is full")
-            print(v.playing)
         end
     end
 end
-while wait() do tpserverless() end
+
+function Teleport()
+    pcall(function()
+        TPReturner()
+        if foundAnything ~= "" then
+            TPReturner()
+        end
+    end)
+end
+
+while wait() do
+    Teleport()
+end
