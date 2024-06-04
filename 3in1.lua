737791f3f91bce078c2244b98add6b284e3b1966
Hlamx <88639120+hlamx@users.noreply.github.com>
2024-06-04 19:26:14 +0700

    Update 3in1.lua

--- 3in1.lua
+++ 3in1.lua
@@ -1,6 +1,6 @@
 repeat task.wait() until game:IsLoaded()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/hlamx/huhu/master/watermark2.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/hlamx/huhu/master/misc.lua"))()
 getgenv().Key = "dfead3a2d23ed448"
 getgenv().NewBanana = true
 loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hlamx/huhu/master/watermark2.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hlamx/huhu/master/misc.lua"))()
