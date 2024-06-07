1e9a4ea59dc28305b73ce960ef248429e2f7edd3
Hlamx <88639120+hlamx@users.noreply.github.com>
2024-06-07 07:50:32 +0700

    Update 3in1.lua

--- 3in1.lua
+++ 3in1.lua
@@ -1,4 +1,24 @@
-repeat task.wait() until game:IsLoaded()
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer:FindFirstChild("DataLoaded")
+repeat
+    pcall(function()
+        if game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("Main"):FindFirstChild("ChooseTeam") then
+            if getgenv().Teams == "Marines" then
+                for r, v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Marines.Frame.TextButton.Activated)) do
+                    v.Function()
+                end
+            elseif getgenv().Teams == "Pirates" then
+                for r, v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Pirates.Frame.TextButton.Activated)) do
+                    v.Function()
+                end
+            else
+                for r, v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Marines.Frame.TextButton.Activated)) do
+                    v.Function()
+                end
+            end
+        end
+    end)
+until game.Players.LocalPlayer.Team ~= nil
+
 spawn(function()
 getgenv().Key = "dfead3a2d23ed448"
 getgenv().NewBanana = false
