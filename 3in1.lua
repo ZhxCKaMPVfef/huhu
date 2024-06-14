9efae8766f7f30fc04ab1881c9644032e6a45e4e
Hlamx <88639120+hlamx@users.noreply.github.com>
2024-06-14 12:34:42 +0700

    Update 3in1.lua

--- 3in1.lua
+++ 3in1.lua
@@ -7,6 +7,7 @@ getgenv().NewBanana = true
 loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))()
   end)
 spawn(function()
+    repeat wait() until game:IsLoaded() and game.Players.LocalPlayer:FindFirstChild("DataLoaded") and game.Players.LocalPlayer.Team ~= nil
     loadstring(game:HttpGet("https://raw.githubusercontent.com/hlamx/huhu/master/watermark2.lua"))()
   end)
 spawn(function()
