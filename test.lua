b2a6340e38ff1f373134583cbe2d4e8af7b02425
Hlamx <88639120+hlamx@users.noreply.github.com>
2024-07-21 18:54:09 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,95 +1,73 @@
-function CheckAcientOneStatus()
-    if not game.Players.LocalPlayer.Character:FindFirstChild("RaceTransformed") then
-        return "You have yet to achieve greatness"
-    end;
-    local v227 = nil;
-    local v228 = nil;
-    local v229 = nil;
-    v229, v228, v227 = game.ReplicatedStorage.Remotes.CommF_:InvokeServer("UpgradeRace", "Check");
-    if v229 == 1 then
-        return "Required Train More"
-    elseif v229 == 2 or v229 == 4 or v229 == 7 then
-        return "Can Buy Gear With " .. v227 .. " Fragments"
-    elseif v229 == 3 then
-        return "Required Train More"
-    elseif v229 == 5 then
-        return "You Are Done Your Race."
-    elseif v229 == 6 then
-        return "Upgrades completed: " .. v228 - 2 .. "/3, Need Trains More"
+function namemoon()
+    if math.floor(game.Lighting.ClockTime) >= 18 or math.floor(game.Lighting.ClockTime) < 5 then
+        return "Time To End ⛅"
+    else
+        return "Time To Night 🌙"
     end
-    if v229 ~= 8 then
-        if v229 == 0 then
-            return "Ready For Trial"
+end
+
+function time()
+    if namemoon() == "Time To Night 🌙" then
+        return 17 - math.floor(game.Lighting.ClockTime)
+    elseif namemoon() == "Time To End ⛅" then
+        if math.floor(game.Lighting.ClockTime) > 0 then
+            return 24 - math.floor(game.Lighting.ClockTime) + 5
         else
-            return "You have yet to achieve greatness"
+            return math.floor(game.Lighting.ClockTime) + 5
         end
     end
-    return "Remaining " .. 10 - v228 .. " training sessions."
 end
 
-local function CheckRace()
-    local bf = game.ReplicatedStorage.Remotes.CommF_:InvokeServer("Wenlocktoad", "1")
-    local c4 = game.ReplicatedStorage.Remotes.CommF_:InvokeServer("Alchemist", "1")
-    if game.Players.LocalPlayer.Character:FindFirstChild("RaceTransformed") then
-        return game:GetService("Players").LocalPlayer.Data.Race.Value .. " V4"
-    end
-    if bf == -2 then
-        return game:GetService("Players").LocalPlayer.Data.Race.Value .. " V3"
-    end
-    if c4 == -2 then
-        return game:GetService("Players").LocalPlayer.Data.Race.Value .. " V2"
-    end
-    return game:GetService("Players").LocalPlayer.Data.Race.Value .. " V1"
-end;
-local d = 0
-function getnumgear()
-    local c = 0
-    for i, v in next, game:GetService("Players").LocalPlayer.Data.Race:GetChildren() do
-        if v.Name ~= "Evolved" then
-            c = c + 1
-        end
-    end
-    return c
-end
-
-spawn(function()
-    while wait() do
-        if
-            game.Players.LocalPlayer.Character:FindFirstChild("RaceEnergy") and
-            game.Players.LocalPlayer.Character.RaceEnergy.Value >= 1 and
-            not game.Players.LocalPlayer.Character.RaceTransformed.Value
-        then
-            d = d + 1
-            repeat
-                wait()
-            until game.Players.LocalPlayer.Character:FindFirstChild("RaceEnergy")
-        end
-    end
-end)
-function checkpoint()
-    if not isfolder("Banana Cat Hub") then
-        makefolder("Banana Cat Hub")
-    end
-    writefile("Banana Cat Hub" .. "/" .. game.Players.LocalPlayer.Name .. ".txt", "Farm Gear 1 Stack Done")
-end
-
---//checkpoint()
-spawn(function()
-    while wait() do
-        if getnumgear() == 1 and d >= 9 and CheckAcientOneStatus() == "Ready For Trial" then
-            checkpoint()
-        end
-    end
-end)
-function checkcantrial()
-    if getnumgear() == 1 then
-        if isfile("Banana Cat Hub" .. "/" .. game.Players.LocalPlayer.Name .. ".txt") or readfile("Banana Cat Hub" .. "/" .. game.Players.LocalPlayer.Name .. ".txt") == "Farm Gear 1 Stack Done" then
-            return true
-        end
-    elseif getnumgear() ~= 1 then
-        if (CheckAcientOneStatus() == "You have yet to achieve greatness" or CheckAcientOneStatus() == "Ready For Trial" or CheckAcientOneStatus() == "You Are Done Your Race.") and (string.find(CheckRace(), "V3") or string.find(CheckRace(), "V4")) then
-            return true
-        end
-    end
-end
-print(checkcantrial())
+local Message = {
+    ["embeds"] = {
+        {
+            ["title"] =
+            "<:bananacon:1261744974534541352>  Banana Hub Notification <:bananacon:1261744974534541352>",
+            ["color"] = 16684576,
+            ["fields"] = {
+                {
+                    ["name"] = namemoon(),
+                    ["value"] = "```" .. time() .. " Minute(s)```",
+                    ["inline"] = true
+                },
+                {
+                    ["name"] = "Time in Server:",
+                    ["value"] = "```\n" .. game.Lighting.TimeOfDay .. "\n```",
+                    ["inline"] = true
+                },
+                {
+                    ["name"] = "Players:",
+                    ["value"] = "```\n" ..
+                        game.Players.NumPlayers .. "/" .. game.Players.MaxPlayers .. "```"
+                },
+                {
+                    ["name"] = "Jobid:",
+                    ["value"] = "```\n" .. game.JobId .. "\n```"
+                },
+                {
+                    ["name"] = "Script To Join:",
+                    ["value"] = "```lua\n" ..
+                    'game:GetService("TeleportService"):TeleportToPlaceInstance(' ..
+                    game.PlaceId .. ',"' .. game.JobId .. '",' .. "game.Players.LocalPlayer" .. "```"
+                }
+            },
+            ["footer"] = {
+                ["text"] = "Banana Hub"
+            },
+            ["timestamp"] = os.date("!%Y-%m-%dT%H:%M:%SZ"),
+            ["thumbnail"] = {
+                ["url"] =
+                "https://cdn.discordapp.com/attachments/1017024488665264218/1262729537578471504/banner_server.jpg?ex=6697a806&is=66965686&hm=e0bd7cbb8460651cc19481bf516ede631dc881b52dcebd9ab54791c37d5dc893&"
+            }
+        }
+    }
+}
+local DataCallBack = request({
+    Url =
+    "https://discord.com/api/webhooks/1012948997884882956/OysNGuyFvGl7UVEAa_eZzNv72FlyXAS1xnsKCdi2ztD973Ud7OXTJLo3W1vuzsJY7Q6u",
+    Method = 'POST',
+    Headers = {
+        ["Content-Type"] = "application/json"
+    },
+    Body = game:GetService("HttpService"):JSONEncode(Message)
+})
