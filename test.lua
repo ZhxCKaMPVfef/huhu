ee492b177d45169c326f48e5ea19d518110973dc
Hlamx <88639120+hlamx@users.noreply.github.com>
2024-08-08 20:19:10 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,73 +1,49 @@
-function namemoon()
-    if math.floor(game.Lighting.ClockTime) >= 18 or math.floor(game.Lighting.ClockTime) < 5 then
-        return "Time To End ⛅"
-    else
-        return "Time To Night 🌙"
+function getypeaccc()
+    local havegod = false
+    local beli = game.Players.LocalPlayer.Data.Beli.Value
+    local frag = game.Players.LocalPlayer.Data.Fragments.Value
+    if game.ReplicatedStorage.Remotes.CommF_:InvokeServer("BuyGodhuman", true) then
+        havegod = true
     end
-end
-
-function time()
-    if namemoon() == "Time To Night 🌙" then
-        return 17 - math.floor(game.Lighting.ClockTime)
-    elseif namemoon() == "Time To End ⛅" then
-        if math.floor(game.Lighting.ClockTime) > 0 then
-            return 24 - math.floor(game.Lighting.ClockTime) + 5
+    local weapon = {}
+    for i, v in next, game:GetService("ReplicatedStorage").Remotes["CommF_"]:InvokeServer("getInventory") do
+        if v.Name == "Soul Guitar" then
+            table.insert(weapon, v.Name)
+        end
+        if v.Name == "Cursed Dual Katana" then
+            table.insert(weapon, v.Name)
+        end
+        if v.Name == "Mirror Fractal" then
+            table.insert(weapon, v.Name)
+        end
+        if v.Name == "Valkyrie Helm" then
+            table.insert(weapon, v.Name)
+        end
+    end
+    if getgenv().checkv3 then
+        if table.find(weapon, "Valkyrie Helm") and table.find(weapon, "Mirror Fractal") and havegod then
+            if table.find(weapon, "Cursed Dual Katana") and table.find(weapon, "Soul Guitar") and beli >= 2500000 and frag >= getgenv().fraglimit and string.find(getgenv().CheckRace(), "V3") then
+                return "GOD CDK SGT MM Have V3"
+            elseif table.find(weapon, "Cursed Dual Katana") and table.find(weapon, "Soul Guitar") and beli >= 2500000 and frag >= getgenv().fraglimit and not string.find(getgenv().CheckRace(), "V3") then
+                return "GOD CDK SGT MM Don't V3"
+            elseif table.find(weapon, "Cursed Dual Katana") and not table.find(weapon, "Soul Guitar") and beli >= 2500000 and frag >= getgenv().fraglimit and string.find(getgenv().CheckRace(), "V3") then
+                return "GOD CDK MM Have V3"
+            elseif table.find(weapon, "Cursed Dual Katana") and not table.find(weapon, "Soul Guitar") and beli >= 2500000 and frag >= getgenv().fraglimit and not string.find(getgenv().CheckRace(), "V3") then
+                return "GOD CDK MM Don't V3"
+            elseif not table.find(weapon, "Cursed Dual Katana") and table.find(weapon, "Soul Guitar") and beli >= 2500000 and frag >= getgenv().fraglimit and string.find(getgenv().CheckRace(), "V3") then
+                return "GOD SGT MM Have V3"
+            elseif not table.find(weapon, "Cursed Dual Katana") and table.find(weapon, "Soul Guitar") and beli >= 2500000 and frag >= getgenv().fraglimit and not string.find(getgenv().CheckRace(), "V3") then
+                return "GOD SGT MM Don't V3"
+            elseif not table.find(weapon, "Cursed Dual Katana") and not table.find(weapon, "Soul Guitar") and beli >= 2500000 and frag >= getgenv().fraglimit and string.find(getgenv().CheckRace(), "V3") then
+                return "GOD MM Have V3"
+            elseif not table.find(weapon, "Cursed Dual Katana") and not table.find(weapon, "Soul Guitar") and beli >= 2500000 and frag >= getgenv().fraglimit and not string.find(getgenv().CheckRace(), "V3") then
+                return "GOD MM Don't Have V3"
+            elseif not table.find(weapon, "Cursed Dual Katana") and not table.find(weapon, "Soul Guitar") and (beli <= 2500000 or frag <= getgenv().fraglimit) then
+                return "Khong Du Dieu Kien"
+            end
         else
-            return math.floor(game.Lighting.ClockTime) + 5
+            return "Khong Du Dieu Kien"
         end
     end
 end
-
-local Message = {
-    ["embeds"] = {
-        {
-            ["title"] =
-            "<:bananacon:1261744974534541352>  Banana Hub Notification <:bananacon:1261744974534541352>",
-            ["color"] = 16684576,
-            ["fields"] = {
-                {
-                    ["name"] = namemoon(),
-                    ["value"] = "```" .. time() .. " Minute(s)```",
-                    ["inline"] = true
-                },
-                {
-                    ["name"] = "Time in Server:",
-                    ["value"] = "```\n" .. game.Lighting.TimeOfDay .. "\n```",
-                    ["inline"] = true
-                },
-                {
-                    ["name"] = "Players:",
-                    ["value"] = "```\n" ..
-                        game.Players.NumPlayers .. "/" .. game.Players.MaxPlayers .. "```"
-                },
-                {
-                    ["name"] = "Jobid:",
-                    ["value"] = "```\n" .. game.JobId .. "\n```"
-                },
-                {
-                    ["name"] = "Script To Join:",
-                    ["value"] = "```lua\n" ..
-                    'game:GetService("TeleportService"):TeleportToPlaceInstance(' ..
-                    game.PlaceId .. ',"' .. game.JobId .. '",' .. "game.Players.LocalPlayer" .. "```"
-                }
-            },
-            ["footer"] = {
-                ["text"] = "Banana Hub"
-            },
-            ["timestamp"] = os.date("!%Y-%m-%dT%H:%M:%SZ"),
-            ["thumbnail"] = {
-                ["url"] =
-                "https://cdn.discordapp.com/attachments/1017024488665264218/1262729537578471504/banner_server.jpg?ex=6697a806&is=66965686&hm=e0bd7cbb8460651cc19481bf516ede631dc881b52dcebd9ab54791c37d5dc893&"
-            }
-        }
-    }
-}
-local DataCallBack = request({
-    Url =
-    "https://discord.com/api/webhooks/1012948997884882956/OysNGuyFvGl7UVEAa_eZzNv72FlyXAS1xnsKCdi2ztD973Ud7OXTJLo3W1vuzsJY7Q6u",
-    Method = 'POST',
-    Headers = {
-        ["Content-Type"] = "application/json"
-    },
-    Body = game:GetService("HttpService"):JSONEncode(Message)
-})
+print(getypeaccc())
