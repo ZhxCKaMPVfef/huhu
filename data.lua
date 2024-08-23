3fc684924495dcdce31959d7508768e321130ec7
Hlamx <88639120+hlamx@users.noreply.github.com>
2024-08-23 10:47:14 +0700

    Create data.lua

---
+++ data.lua
@@ -0,0 +1,13 @@
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer:FindFirstChild("DataLoaded")
+getgenv().MainAccount = {
+    "bocanhet164",
+    "Phamtram0rfqU"
+}
+getgenv().truonghopdatbiet = {
+    "ViMhZQhJTqv"
+}
+getgenv().fraglimit = 0
+
+getgenv().Race = "Random" -- Human , Skypiea , Mink, Fishman, Random
+getgenv().blacklistrace = { "Fishman", "Skypiea", "Mink" }
+getgenv().gear = 5        -- 1, 5
