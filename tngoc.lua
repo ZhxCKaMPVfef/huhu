6218abd0bca840b6e618e235efaa2248fb20590a
Hlamx <88639120+hlamx@users.noreply.github.com>
2024-08-30 13:08:09 +0700

    Update tngoc.lua

--- tngoc.lua
+++ tngoc.lua
@@ -60,8 +60,6 @@ getgenv().PlayerAdded = function(plr)
                     if not table.find(getgenv().pass, plr) then
                         table.insert(getgenv().pass, plr)
                         task.wait(1)
-                        print(plr.Name, content)
-                        print(#getgenv().save, #getgenv().pass)
                     end
                 elseif content == "Start" then
                     game.ReplicatedStorage.Remotes.CommE:FireServer("ActivateAbility")
