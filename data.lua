0408dc9983bf4358154f9089a5df922f7689fcf3
Hlamx <88639120+hlamx@users.noreply.github.com>
2024-09-09 19:50:16 +0700

    Update data.lua

--- data.lua
+++ data.lua
@@ -7,7 +7,7 @@ getgenv().truonghopdatbiet = {
     "ViMhZQhJTqv"
 }
 getgenv().fraglimit = 0
-getgenv().link = "http://192.168.1.4:2908"
+getgenv().link = "http://192.168.1.5:2908"
 getgenv().Race = "Random" -- Human , Skypiea , Mink, Fishman, Random
 getgenv().blacklistrace = { "Fishman", "Skypiea", "Mink" }
 getgenv().gear = 5        -- 1, 5
