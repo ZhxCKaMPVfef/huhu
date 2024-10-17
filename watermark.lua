655d7ff9009e2dda27556e64a530beefbc7328ef
Hlamx <88639120+ZhxCKaMPVfef@users.noreply.github.com>
2024-10-17 17:34:12 +0700

    Update watermark.lua

--- watermark.lua
+++ watermark.lua
@@ -1,37 +1,4 @@
-repeat wait() until game:IsLoaded() and game.Players.LocalPlayer:FindFirstChild("DataLoaded")
-repeat
-    pcall(
-        function()
-            if game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("Main"):FindFirstChild("ChooseTeam") then
-                if getgenv().Teams == "Marines" then
-                    for r, v in pairs(
-                        getconnections(
-                            game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Marines.Frame.TextButton.Activated
-                        )
-                    ) do
-                        v.Function()
-                    end
-                elseif getgenv().Teams == "Pirates" then
-                    for r, v in pairs(
-                        getconnections(
-                            game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Pirates.Frame.TextButton.Activated
-                        )
-                    ) do
-                        v.Function()
-                    end
-                else
-                    for r, v in pairs(
-                        getconnections(
-                            game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Marines.Frame.TextButton.Activated
-                        )
-                    ) do
-                        v.Function()
-                    end
-                end
-            end
-        end
-    )
-until game.Players.LocalPlayer.Team ~= nil
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer:FindFirstChild("DataLoaded") and game.Players.LocalPlayer.Team ~= nil
 game:service("VirtualInputManager"):SendKeyEvent(true, "Tab", false, game)
 wait(0)
 game:service("VirtualInputManager"):SendKeyEvent(false, "Tab", false, game)
